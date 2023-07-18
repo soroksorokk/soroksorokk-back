c6215d1d6fddc57e3531b004e8ebf40368084626
Gwanho KIM <bigquan977@gmail.com>
2023-07-18 15:02:42 +0900

    [INIT]: Initiate Project

---
+++ src/main/java/sorok/soroksorok/SoroksorokApplication.java
@@ -0,0 +1,13 @@
+package sorok.soroksorok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoroksorokApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SoroksorokApplication.class, args);
+  }
+
+}
