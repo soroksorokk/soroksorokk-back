5ee7af85820549f88c18a03632c6af1ae6ba3e9f
Gwanho KIM <bigquan977@gmail.com>
2023-07-19 23:03:56 +0900

    [CHORE]: Test 주석 삭제

--- src/test/java/sorok/soroksorok/SoroksorokApplicationTests.java
+++ src/test/java/sorok/soroksorok/SoroksorokApplicationTests.java
@@ -1,13 +1,13 @@
-//package sorok.soroksorok;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class SoroksorokApplicationTests {
-//
-//  @Test
-//  void contextLoads() {
-//  }
-//
-//}
+package sorok.soroksorok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoroksorokApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
