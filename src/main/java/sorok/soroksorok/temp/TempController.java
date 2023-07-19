a3d471b5966fde4b1bc97e1d2cf4ce2ceda5d95b
Gwanho KIM <bigquan977@gmail.com>
2023-07-19 23:08:48 +0900

    [CHORE]: 연결 확인용 temp 생성

--- src/main/java/sorok/soroksorok/temp/TempController.java
+++ src/main/java/sorok/soroksorok/temp/TempController.java
@@ -1,12 +1,13 @@
 package sorok.soroksorok.temp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TempController {
 
-  @GetMapping("/temporary")
+  @GetMapping("/temp")
   public String temporary() {
     return "연결 완료";
   }
