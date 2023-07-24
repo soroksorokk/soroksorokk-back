65e1d0241a7fd4ea87c0c5d58c21f90d6eee6c82
Gwanho KIM <bigquan977@gmail.com>
2023-07-24 12:40:51 +0900

    [CHORE]: Test 주석 처리

--- src/test/java/sorok/soroksorok/SoroksorokApplicationTests.java
+++ src/test/java/sorok/soroksorok/SoroksorokApplicationTests.java
@@ -3,11 +3,11 @@ package sorok.soroksorok;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SoroksorokApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
-
-}
+//@SpringBootTest
+//class SoroksorokApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//
+//}
