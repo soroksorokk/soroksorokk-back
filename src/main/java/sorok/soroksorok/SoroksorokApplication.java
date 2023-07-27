e0c55fef694b28999cf2e545e47f2715cef1e6c4
Gwanho KIM <bigquan977@gmail.com>
2023-07-27 09:36:17 +0900

    [FIX]: Swagger EC2 CORS 문제 해결

--- src/main/java/sorok/soroksorok/SoroksorokApplication.java
+++ src/main/java/sorok/soroksorok/SoroksorokApplication.java
@@ -1,11 +1,14 @@
 package sorok.soroksorok;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://bigquann.shop", description = "Default Server URL")})
 public class SoroksorokApplication {
 
   public static void main(String[] args) {
