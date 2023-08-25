00c5599f1772aa4a1bf8d2b3c15e4ec9a9b7bcfa
Gwanho KIM <bigquan977@gmail.com>
2023-08-25 14:20:22 +0900

    [FEAT]: OAuth 회원가입 닉네임 생성기 적용

--- src/main/java/sorok/soroksorok/global/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/sorok/soroksorok/global/oauth2/OAuth2LoginSuccessHandler.java
@@ -10,6 +10,8 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 import sorok.soroksorok.global.jwt.JwtService;
 import sorok.soroksorok.user.entity.Role;
+import sorok.soroksorok.user.entity.User;
+import sorok.soroksorok.user.repository.UserRepository;
 
 @Slf4j
 @Component
@@ -18,7 +20,7 @@ import sorok.soroksorok.user.entity.Role;
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
   private final JwtService jwtService;
-//    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
@@ -47,10 +49,17 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
   // TODO : 소셜 로그인 시에도 무조건 토큰 생성하지 말고 JWT 인증 필터처럼 RefreshToken 유/무에 따라 다르게 처리해보기
   private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) {
+
+    User user = userRepository.findByEmail(oAuth2User.getEmail())
+        .orElseThrow(RuntimeException::new);
+
     String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
     String refreshToken = jwtService.createRefreshToken();
+    String nickname = user.getNickname();
+
     response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
     response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
+    response.addHeader("nickname", nickname);
 
     jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
     jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
--- src/main/java/sorok/soroksorok/global/oauth2/OAuthAttributes.java
+++ src/main/java/sorok/soroksorok/global/oauth2/OAuthAttributes.java
@@ -78,15 +78,17 @@ public class OAuthAttributes {
    * of메소드로 OAuthAttributes 객체가 생성되어, 유저 정보들이 담긴 OAuth2UserInfo가 소셜 타입별로 주입된 상태
    * OAuth2UserInfo에서 socialId(식별값), nickname, imageUrl을 가져와서 build
    * email에는 UUID로 중복 없는 랜덤 값 생성
-   * role은 GUEST로 설정
+   * role은 USER로 설정
    */
   public User toEntity(SocialType socialType, OAuth2UserInfo oauth2UserInfo) {
     return User.builder()
         .socialType(socialType)
         .socialId(oauth2UserInfo.getId())
         .email(UUID.randomUUID() + "@socialUser.com")
-        .nickname(oauth2UserInfo.getNickname())
-        .imageUrl(oauth2UserInfo.getImageUrl())
+        .tags("")
+        .description("")
+        .nickname(nicknamingUtils.makeRandomNaming())
+        .imageUrl("default.jpg")
         .role(Role.USER)
         .build();
   }
