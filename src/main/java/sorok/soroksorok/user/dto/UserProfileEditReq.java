14563689a354e60cbb927fc1ce72a789439de655
Gwanho KIM <bigquan977@gmail.com>
2023-09-09 14:20:22 +0900

    [FEAT]: User API 문서화

--- src/main/java/sorok/soroksorok/user/dto/UserProfileEditReq.java
+++ src/main/java/sorok/soroksorok/user/dto/UserProfileEditReq.java
@@ -1,16 +1,22 @@
 package sorok.soroksorok.user.dto;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @ApiModel(value = "유저 프로필 수정 요청")
 @Getter
+@NoArgsConstructor
 public class UserProfileEditReq {
 
+  @ApiModelProperty(value="닉네임", example = "nickname", required = true)
   private String nickname;
 
+  @ApiModelProperty(value="태그", example = "가나다/라마바/사아자", required = true)
   private String tags;
 
+  @ApiModelProperty(value="설명", example = "설명입니다.", required = true)
   private String description;
 
 }
--- src/main/java/sorok/soroksorok/user/dto/UserProfileRes.java
+++ src/main/java/sorok/soroksorok/user/dto/UserProfileRes.java
@@ -1,6 +1,7 @@
 package sorok.soroksorok.user.dto;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import sorok.soroksorok.user.entity.User;
@@ -10,18 +11,25 @@ import sorok.soroksorok.user.entity.User;
 @Builder
 public class UserProfileRes {
 
+  @ApiModelProperty(value="설명", example = "설명입니다.", required = true)
   private String description;
 
+  @ApiModelProperty(value="태그", example = "가나다/라마바/사아자", required = true)
   private String tags;
 
+  @ApiModelProperty(value="이메일", example = "nickname@email.com", required = true)
   private String email;
 
+  @ApiModelProperty(value="프로필 이미지", example = "URL", required = true)
   private String imageUrl;
 
+  @ApiModelProperty(value="닉네임", example = "nickname", required = true)
   private String nickname;
 
+  @ApiModelProperty(value="팔로워 숫자", example = "1", required = true)
   private Long followerCnt;
 
+  @ApiModelProperty(value="팔로잉 숫자", example = "1", required = true)
   private Long followingCnt;
 
   public static UserProfileRes of(User user, Long followerCnt, Long followingCnt) {
