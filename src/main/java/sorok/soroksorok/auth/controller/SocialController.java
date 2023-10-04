ce84cd692693bb03d246b7e392108e7f41648cad
Gwanho KIM <bigquan977@gmail.com>
2023-10-04 09:58:41 +0900

    [FEAT]: Add Social Swagger

--- src/main/java/sorok/soroksorok/auth/controller/SocialController.java
+++ src/main/java/sorok/soroksorok/auth/controller/SocialController.java
@@ -4,11 +4,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @Component
 @Api(tags = "Social Auth API - 인증/인가")
+@RestController
 public class SocialController {
 
   @ApiOperation(
@@ -16,6 +20,7 @@ public class SocialController {
   @ApiResponses({
       @ApiResponse(responseCode = "200", description = "토큰 재발급 성공") })
   @GetMapping("/oauth2/authorization/google")
+  @ResponseStatus(HttpStatus.CREATED)
   public void googleLogin() {
   }
 
